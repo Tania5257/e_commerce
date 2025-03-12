05659cd206dab352d2c8aa35a04d052abfd828d4
Tania5257 <tania15-5257@diu.edu.bd>
2025-03-12 10:15:26 +0600

    ec

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  static const primary = Color(0xFF5CC936);
+  static const secondary = Color(0xFFFFFFFF);
+  static const yel = Color(0xFFFFC120);
+  static const black = Color(0xFF0c1A30);
+}
+//FFFFFF
